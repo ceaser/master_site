b1892e4f88753403dac96b92cca0fa12dd0acc54
Ceaser Larry <clarry@divergentlogic.com>
2011-12-18 22:59:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasterSite::Application.config.secret_token = '28dab620a0e070fff60f182e9be112388ea94293a7502b4ff770bb99f7e3e8988255b8d57c0773fbba31952d238e85586ac54aeb087e7295c61eb8d65eca9418'
